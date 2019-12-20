3d63e01ad5a5145548413835ae53352f44c349da
Facundo Competiello <fcompetiello@fcompetiello.local>
2019-12-20 07:52:43 -0300

    Unify book resolver

--- src/main/java/facu/graphql/demo/resolvers/BookResolver.java
+++ src/main/java/facu/graphql/demo/resolvers/BookResolver.java
@@ -1,18 +1,30 @@
 package facu.graphql.demo.resolvers;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import facu.graphql.demo.enumerators.Genre;
 import facu.graphql.demo.models.Book;
 import facu.graphql.demo.repositories.BookRepository;
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BookResolver implements GraphQLQueryResolver {
+public class BookResolver implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private BookRepository bookRepository;
 
     public Book bookById(Integer id) {
         return bookRepository.findById(id).get();
+    }
+
+    // TODO: Input types are not working. However information can be parsed from dataFetchingEnvironment
+    public Book createBook(String name, Integer pageCount, Genre genre, DataFetchingEnvironment dataFetchingEnvironment){
+        Book book = new Book();
+        book.setName(name);
+        book.setPageCount(pageCount);
+        book.setGenre(genre);
+        return bookRepository.save(book);
     }
 }
